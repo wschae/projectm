db2c999789a1f22da5c991e98a7e0faba6d5db31
Wonseok Chae <wochae@microsoft.com>
2018-08-26 10:23:49 -0700

    raise puzzle difficulty (#3)
    * raise puzzle difficulty
    * cleanup code

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.use(express.static(__dirname + "/client/build"));
 
 app.get('/api/people', (req, res) => {
     var people = [
-        {name: 'jwkhong', url: 'assets/jwkhong.jpg', nonce: 5076},
-        {name: 'eyyoun', url: 'assets/eyyoun.gif', nonce: 40764},
-        {name: 'hwanjoyu', url: 'assets/hwanjoyu.gif', nonce: 14418},
-        {name: 'hyelee', url: 'assets/hyelee.jpg', nonce: 26529},
-        {name: 'wschae', url: 'assets/wschae.jpg', nonce: 18776},
+        {name: 'jwkhong', url: 'assets/jwkhong.jpg', nonce: 58702553},
+        {name: 'eyyoun', url: 'assets/eyyoun.gif', nonce: 89239464},
+        {name: 'hwanjoyu', url: 'assets/hwanjoyu.gif', nonce: 120291891},
+        {name: 'hyelee', url: 'assets/hyelee.jpg', nonce: 46326056},
+        {name: 'wschae', url: 'assets/wschae.jpg', nonce: 354861675},
     ];
 
   res.json(people);
