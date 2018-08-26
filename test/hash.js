d772e63215142df9ee7b311b5d2e73044ab92bb9
Wonseok Chae <wochae@microsoft.com>
2018-08-26 09:03:31 -0700

    raise puzzle difficulty

--- test/hash.js
+++ test/hash.js
@@ -1,8 +1,8 @@
 const bitcoin = require('bitcoinjs-lib');
 const fs = require('fs')
 
-// const difficulty = '0000000';
-const difficulty = '0000';
+const difficulty = '0000000';
+
 const path = __dirname + '/../client/public/';
 
 const computeSha256 = (data) => bitcoin.crypto.sha256(data);
