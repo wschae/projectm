e960173fc3cb5d370ee3d1ab85e4c3adca878f87
kyungchan <kkc90@postech.ac.kr>
2018-09-04 17:12:00 +0900

    add a new photo and find my nonce

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/api/people', (req, res) => {
         {name: 'hwanjoyu', url: 'assets/hwanjoyu.gif', nonce: '120291891'},
         {name: 'hyelee', url: 'assets/hyelee.jpg', nonce: '46326056'},
         {name: 'wschae', url: 'assets/wschae.jpg', nonce: '354861675'},
+        {name: 'kyungchan', url: 'assets/kyungchan.jpg', nonce: '359310174'},        
     ];
 
   res.json(people);
