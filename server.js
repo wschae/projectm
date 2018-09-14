752f44e97db8891fc130aba7657ab0f45786858d
sophianetSun <33086273+sophianetSun@users.noreply.github.com>
2018-09-14 23:57:36 +0900

    add a new memeber swkim (#7)

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.get('/api/people', (req, res) => {
         {name: 'hwanjoyu', url: 'assets/hwanjoyu.gif', nonce: '120291891'},
         {name: 'hyelee', url: 'assets/hyelee.jpg', nonce: '46326056'},
         {name: 'wschae', url: 'assets/wschae.jpg', nonce: '354861675'},
-        {name: 'kyungchan', url: 'assets/kyungchan.jpg', nonce: '359310174'},        
+        {name: 'kyungchan', url: 'assets/kyungchan.jpg', nonce: '359310174'},
+        {name: 'swkim', url: 'assets/swkim.jpg', nonce: '244859547'}
     ];
 
   res.json(people);
